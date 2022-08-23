2f9e303ed424d55b70bb1a830ed36a89a78d7413
Tsypin Ilya <ilyaboy@list.ru>
2022-08-23 12:50:44 +0300

    Add auto migration db when app is starting

---
+++ src/FavoriteLiterature.Api/Extensions/ApplicationBuilderExtension.cs
@@ -0,0 +1,23 @@
+﻿using FavoriteLiterature.Api.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace FavoriteLiterature.Api.Extensions;
+
+public static class ApplicationBuilderExtension
+{
+    public static IApplicationBuilder UseMigrationOfDbContext<T>(this IApplicationBuilder builder) where T : DataContext
+    {
+        using var scope = builder.ApplicationServices.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<T>();
+        var migrations = context.Database.GetPendingMigrations().ToArray();
+
+        if (!migrations.Any())
+        {
+            return builder;
+        }
+        
+        context.Database.Migrate();
+        
+        return builder;
+    }
+}
